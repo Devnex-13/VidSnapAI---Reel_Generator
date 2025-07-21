35fa121c82e783aafd25efdf147543db5e901e7a
Devnex-13 <devanshu.p.pote@gmail.com>
2025-07-22 02:41:29 +0530

    Add The Files Inside User_Upload Folder and Add Function of Uploading Files to the server in Flask

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from flask import Flask, render_template,request
 import uuid
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = 'user_upload'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def home():
@@ -12,9 +18,22 @@ def create():
     myid = uuid.uuid1()
     if request.method == "POST":
         print(request.files.keys())
-        for key,values in request.files.items():
-            print(key,values)
-    return render_template("create.html")
+        rec_id = request.form.get("uuid")
+        desc = request.form.get("text")
+        for key,value in request.files.items():
+            print(key,value)
+            file = request.files[key]
+            if file:
+                filename = secure_filename(file.filename)
+                if not os.path.exists(os.path.join(app.config['UPLOAD_FOLDER'], rec_id)):
+                    os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'], rec_id))
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], rec_id, filename))
+            with open(os.path.join(app.config['UPLOAD_FOLDER'], rec_id, "desc.txt"), "w") as f:
+                f.write(desc)
+
+        
+
+    return render_template("create.html", myid=myid)
 
 @app.route("/gallery")
 def gallery():
